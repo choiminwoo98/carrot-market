7b3f2d20fd0c908e3b8f48d776d3e15b8ce332c7
choiminwoo98 <cmw0918@naver.com>
2022-11-01 21:49:12 +0900

    Streams

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,19 +2,90 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { User } from "@prisma/client";
+import useMutation from "@libs/client/useMutation";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  name?: string;
+  avatar?: string;
+  formErrors?: string;
+}
+
+interface EditProfileRespone {
+  ok: boolean;
+  error?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+    watch,
+  } = useForm<EditProfileForm>();
+  useEffect(() => {
+    if (user?.name) setValue("name", user.name);
+
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+  const [editProfile, { data, loading }] =
+    useMutation<EditProfileRespone>(`/api/users/me`);
+  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
+    console.log(avatar);
+    return;
+    if (loading) return;
+    if (email === "" && phone === "" && name === "") {
+      setError("formErrors", {
+        message: "Emamil OR Phone number are required",
+      });
+    }
+    editProfile({
+      email,
+      phone,
+      name,
+    });
+  };
+  useEffect(() => {
+    if (data && !data.ok && data.error) {
+      setError("formErrors", { message: data.error });
+    }
+  }, [data, setError]);
+  const [avatarPreview, setAvatarPreview] = useState("");
+  const avatar = watch("avatar");
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      const file = avatar[0];
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  }, [avatar]);
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="py-10 px-4 space-y-4">
+      <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className="w-14 h-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <div className="w-14 h-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
@@ -22,15 +93,35 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
+
         <Input
-          required
+          required={false}
+          register={register("name")}
+          label="Name"
+          name="name"
+          type="text"
+        />
+        <Input
+          required={false}
+          register={register("email")}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          required={false}
+          register={register("phone")}
           label="Phone number"
           name="phone"
           type="number"
           kind="phone"
         />
-        <Button text="Update profile" />
+        {errors.formErrors ? (
+          <span className="my-2 text-red-500 font-bold">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
+        <Button text={loading ? "Loading..." : "Update profile"} />
       </form>
     </Layout>
   );
